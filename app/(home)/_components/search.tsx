c5599e9e5640dcf143a7652fd5742d0ff745a6e6
glaydson-alves <glaydson.alves36@gmail.com>
2024-02-01 17:16:11 -0300

    feat: add component de search

---
+++ app/(home)/_components/search.tsx
@@ -0,0 +1,18 @@
+"use client" // é um client components que siginifica que haverá interação com usuário quando clicar ou escrever/ pode usar state
+
+import { Input } from "@/app/_components/ui/input"
+import { Button } from "@/app/_components/ui/button"
+import { SearchIcon } from "lucide-react"
+
+const Search = () => {
+    return(
+        <div className="flex items-center gap-2">
+            <Input placeholder="Busque por uma barbearia..." />
+            <Button variant="default">
+                <SearchIcon size={18} />
+            </Button>
+        </div>
+    )
+}
+
+export default Search 
