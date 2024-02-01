e716ea7811052aac4c0db3695b64e0e9d812b8f2
glaydson-alves <glaydson.alves36@gmail.com>
2024-02-01 19:46:05 -0300

    feat: add footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="w-full bg-secondary py-6 px-5">
+            <p className="text-gray-400 text-xs font-bold opacity-75 text-center">&copy; 2024 Copyright Glaydson Alves.</p>
+        </div>
+    );
+}
+ 
+export default Footer;
