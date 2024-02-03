6c9d09f98a4a49831e255b16f191cc2fb1f9a9a3
glaydson-alves <glaydson.alves36@gmail.com>
2024-02-03 03:09:53 -0300

    feat: add componente de sidemenu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import { LogInIcon, LogOutIcon, MenuIcon, CircleUserIcon, HomeIcon, CalendarIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import Image from "next/image";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Avatar, AvatarImage } from "./ui/avatar";
-import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
 const Header = () => {
-    const {data, status} = useSession();
-    const handleLoginClick = async ()=>{
-        await signIn("google");
-    }
-    const handleLogoutClick = () => signOut();
     return ( 
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -28,59 +21,10 @@ const Header = () => {
                     </SheetTrigger>
 
                     <SheetContent className="p-0">
-                        <SheetHeader className="text-left border-b border-solid border-secondary p-5">
-                            <SheetTitle>Menu</SheetTitle>
-                        </SheetHeader>
-
-                        {data?.user ? (
-                            <div className="flex justify-between px-5 py-6 items-center">
-                                <div className="flex items-center gap-3">
-                                    <Avatar>
-                                        <AvatarImage src={data.user?.image ?? ""}/>
-                                    </Avatar>
-
-                                    <h2 className="font-bold">{data.user.name}</h2>
-                                </div>
-                                <Button variant="secondary" size="icon">
-                                    <LogOutIcon onClick={handleLogoutClick}/>
-                                </Button>
-                            </div>
-                            
-                        ) : (
-                            <div className="flex flex-col px-5 py-6 gap-3">
-                                <div className="flex items-center gap-2">
-                                    <CircleUserIcon size={32} className="text-gray-400"/>
-                                    <h2 className="font-bold">Olá, faça seu login!</h2>
-                                </div>
-                                <Button variant="secondary" className="w-full justify-start" onClick={handleLoginClick}>
-                                    <LogInIcon className="mr-2" size={18}/>
-                                    Fazer Login
-                                </Button>
-                            </div>
-                        )}
-
-                        <div className="flex flex-col gap-3 px-5">
-                            <Button variant="outline" className="justify-start" asChild>
-                                <Link href="/">
-                                    <HomeIcon size={18} className="mr-2"/>
-                                    Início
-                                </Link>
-                            </Button>
-
-                            {data?.user && (
-                                <Button variant="outline" className="justify-start" asChild>
-                                    <Link href="/bookings">
-                                        <CalendarIcon size={18} className="mr-2"/>
-                                        Agendamentos
-                                    </Link>
-                                </Button>
-
-                            )}
-
-                        </div>
+                        <SideMenu/>
                     </SheetContent>
 
-                </Sheet>              
+                </Sheet>      
                 
             </CardContent>
         </Card>
