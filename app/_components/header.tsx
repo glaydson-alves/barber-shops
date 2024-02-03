3d7846bc83a5463f1ce31a04cfa5dd018d91c4b8
glaydson-alves <glaydson.alves36@gmail.com>
2024-02-03 01:18:33 -0300

    feat: add provedor de autenticação com o google

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { signIn, useSession } from "next-auth/react";
 
 const Header = () => {
+    // const { data } useSession();
+
+    const handleLoginClick = async ()=>{
+        await signIn();
+    }
     return ( 
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -12,6 +20,8 @@ const Header = () => {
                 <Button variant="outline" size="icon">
                     <MenuIcon size={18} />
                 </Button>
+                
+                <Button onClick={handleLoginClick}>Login</Button>
             </CardContent>
         </Card>
      );
